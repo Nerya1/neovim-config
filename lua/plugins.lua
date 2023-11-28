ca78541a727a2e92afb5becec1f6b6a92b620b3e
Nerya Madar <thatneryamadar@gmail.com>
2023-11-28 21:32:46 +0200

    notes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -72,5 +72,26 @@ require('lazy').setup({
       style = "night",
     },
   },
+  {
+    'renerocksai/telekasten.nvim',
+    config = {
+      home = vim.fn.expand("~/notes"),
+    },
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'renerocksai/calendar-vim',
+      {
+        "toppair/peek.nvim",
+        event = { "VeryLazy" },
+        build = "deno task --quiet build:fast",
+        config = function()
+            require("peek").setup()
+            -- refer to `configuration to change defaults`
+            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        end,
+      },
+    },
+  },
 })
 
